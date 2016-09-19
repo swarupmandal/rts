05e9c7ad5d924a488b52a5e8cc699805bd54e249
swarup <swarup12x@gmail.com>
2016-09-19 14:44:11 +0530

    #

--- rts/src/org/appsquad/database/DbConnection.java
+++ rts/src/org/appsquad/database/DbConnection.java
@@ -1,5 +1,33 @@
 package org.appsquad.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DbConnection {
+
+
+	public static Connection createConnection(){
+		Connection connection = null;
+		try {
+			
+			Class.forName("org.postgresql.Driver");
+			
+			connection = DriverManager.getConnection(DbConstants.JDBCURL, DbConstants.USERNAME,DbConstants.PASSWORD);			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		System.out.println("Connection ::"+connection);
+		return connection;
+	}
+	
+	/*public static void main(String[] args) {
+		createConnection();
+	}*/
+	
+
+	
 
 }
--- rts/src/org/appsquad/database/DbConstants.java
+++ rts/src/org/appsquad/database/DbConstants.java
@@ -2,4 +2,14 @@ package org.appsquad.database;
 
 public class DbConstants {
 
+	public static final String DRIVERCLASS = "org.postgresql.Driver";
+	public static final String DBNAME="rts";
+	
+//	public static final String JDBCURL = "jdbc:postgresql://192.168.0.1:5432/"+DBNAME;
+	public static final String JDBCURL = "jdbc:postgresql://localhost:5432/"+DBNAME;
+	
+	public static final String USERNAME = "postgres";
+	public static final String PASSWORD = "password";
+
+	
 }
