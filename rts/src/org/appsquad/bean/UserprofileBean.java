667f152b69ad91bbf050d433e7f44ccd8f61fbc1
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-20 20:56:43 +0530

    #

--- rts/src/org/appsquad/bean/UserprofileBean.java
+++ rts/src/org/appsquad/bean/UserprofileBean.java
@@ -1,80 +1,57 @@
 package org.appsquad.bean;
 
-import java.util.ArrayList;
-
 public class UserprofileBean {
 
 	private String username;
 	private String password;
-	private Integer userid;
+	private String userid;
 	private String email;
 	private String address;
 	private String Contactno;
+	private int id;
 	
+	/************************************************************************************************************************************/
 	
-	
-	public String getPassword() {
-		return password;
-	}
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-	public String getAddress() {
-		return address;
-	}
-
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-
-	public String getContactno() {
-		return Contactno;
-	}
-
-
-	public void setContactno(String contactno) {
-		Contactno = contactno;
-	}
-
-
-	public Integer getUserid() {
-		return userid;
-	}
-
-
-	public void setUserid(Integer userid) {
-		this.userid = userid;
-	}
-
-
-		
-	
-
-
 	public String getUsername() {
 		return username;
 	}
-
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-
+	public String getPassword() {
+		return password;
 	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactno() {
+		return Contactno;
+	}
+	public void setContactno(String contactno) {
+		Contactno = contactno;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
--- rts/src/org/appsquad/viewmodel/UserprofileViewModel.java
+++ rts/src/org/appsquad/viewmodel/UserprofileViewModel.java
@@ -1,83 +1,102 @@
 package org.appsquad.viewmodel;
 
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.appsquad.bean.UserprofileBean;
+import org.appsquad.dao.UserProfileDao;
+import org.appsquad.service.UserProfileService;
 import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.GlobalCommand;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Session;
 import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zul.Window;
 
 public class UserprofileViewModel {
-	Session session = null;
-
 	UserprofileBean userprofileBean=new UserprofileBean();
 	
-	ArrayList<UserprofileBean> userbeanlList= new ArrayList<UserprofileBean>();
+	private Connection connection = null;
+	private Session sessions = null;
+	private String userName ;
+	private String userId;
 	
-	
-	public Session getSession() {
-		return session;
-	}
-
-
-	public void setSession(Session session) {
-		this.session = session;
-	}
-
+	private ArrayList<UserprofileBean> userList = new ArrayList<UserprofileBean>();
 
 	@AfterCompose
 	public void initSetUp(@ContextParam(ContextType.VIEW) Component view)
 			throws Exception {
-
 		Selectors.wireComponents(view, this, false);
+		sessions = Sessions.getCurrent();
+		userList = UserProfileDao.onLoadUserDeatils();
+	}
 	
-		session = Sessions.getCurrent();
-		
-	}
-
-		
-	@Command
+	@GlobalCommand
 	@NotifyChange("*")
-	public void onSelectusername()
-	{
-		
+	public void globalUserDetailsUpdate(){
+		userList = UserProfileDao.onLoadUserDeatils();
 	}
-	@Command
-	@NotifyChange("*")
-	public void onSelectuserrole()
-	{
-		
-	}
-	@Command
-	@NotifyChange("*")
-	public void onClickroleExisting(){
-		
-	}
+	
 	@Command
 	@NotifyChange("*")
 	public void onClickuserSubmit(){
-		
+		UserProfileService.insertUserMasterData(userprofileBean);
+		UserProfileService.clearAllField(userprofileBean);
 	}
+	
+	@Command
+	@NotifyChange("*")
+	public void onClickUpdateButton(@BindingParam("bean") UserprofileBean userprofileBean){
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("userIdDetails", userprofileBean);
+		Window window = (Window) Executions.createComponents("/WEB-INF/view/userInformationUpdate.zul", null, map);
+		window.doModal();
+	}
+
+	/***************************************************Getter And Setter Method ****************************************************************/
+    
 	public UserprofileBean getUserprofileBean() {
 		return userprofileBean;
 	}
-
 	public void setUserprofileBean(UserprofileBean userprofileBean) {
 		this.userprofileBean = userprofileBean;
 	}
-
-	public ArrayList<UserprofileBean> getUserbeanlList() {
-		return userbeanlList;
+	public Connection getConnection() {
+		return connection;
 	}
-
-	public void setUserbeanlList(ArrayList<UserprofileBean> userbeanlList) {
-		this.userbeanlList = userbeanlList;
+	public void setConnection(Connection connection) {
+		this.connection = connection;
 	}
-
+	public Session getSessions() {
+		return sessions;
+	}
+	public void setSessions(Session sessions) {
+		this.sessions = sessions;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public ArrayList<UserprofileBean> getUserList() {
+		return userList;
+	}
+	public void setUserList(ArrayList<UserprofileBean> userList) {
+		this.userList = userList;
+	}
 }
