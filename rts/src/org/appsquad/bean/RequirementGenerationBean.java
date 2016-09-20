b40cad4d0694537bc1d77b6562e7a63a9f907d3c
swarup <swarup12x@gmail.com>
2016-09-20 16:44:09 +0530

    # Req Gen Viewmodel

---
+++ rts/src/org/appsquad/bean/RequirementGenerationBean.java
@@ -0,0 +1,179 @@
+package org.appsquad.bean;
+
+import java.util.Date;
+
+public class RequirementGenerationBean {
+
+  private String clientName;
+  private String clientfName;
+  private String clientsName;
+  private int clientId;
+  
+  private String reqSkill;
+  private int reqSkillId;
+  
+  private String jobType;
+  
+  private String detailedJob;
+  
+  private int nofPerResource;
+  
+  private int nofConResource;
+  
+  private Date raiseDate;
+  private String raiseDateStr;
+  
+  private Date closeDate;
+  private String closeDateStr;
+  
+  private String reqStatus;
+  private int reqStatusId;
+  
+  private String closureReason;
+  
+  private String userName;
+
+public String getClientName() {
+	return clientName;
+}
+
+public void setClientName(String clientName) {
+	this.clientName = clientName;
+}
+
+public String getClientfName() {
+	return clientfName;
+}
+
+public void setClientfName(String clientfName) {
+	this.clientfName = clientfName;
+}
+
+public String getClientsName() {
+	return clientsName;
+}
+
+public void setClientsName(String clientsName) {
+	this.clientsName = clientsName;
+}
+
+public int getClientId() {
+	return clientId;
+}
+
+public void setClientId(int clientId) {
+	this.clientId = clientId;
+}
+
+public String getReqSkill() {
+	return reqSkill;
+}
+
+public void setReqSkill(String reqSkill) {
+	this.reqSkill = reqSkill;
+}
+
+public int getReqSkillId() {
+	return reqSkillId;
+}
+
+public void setReqSkillId(int reqSkillId) {
+	this.reqSkillId = reqSkillId;
+}
+
+public String getJobType() {
+	return jobType;
+}
+
+public void setJobType(String jobType) {
+	this.jobType = jobType;
+}
+
+public String getDetailedJob() {
+	return detailedJob;
+}
+
+public void setDetailedJob(String detailedJob) {
+	this.detailedJob = detailedJob;
+}
+
+public int getNofPerResource() {
+	return nofPerResource;
+}
+
+public void setNofPerResource(int nofPerResource) {
+	this.nofPerResource = nofPerResource;
+}
+
+public int getNofConResource() {
+	return nofConResource;
+}
+
+public void setNofConResource(int nofConResource) {
+	this.nofConResource = nofConResource;
+}
+
+public Date getRaiseDate() {
+	return raiseDate;
+}
+
+public void setRaiseDate(Date raiseDate) {
+	this.raiseDate = raiseDate;
+}
+
+public String getRaiseDateStr() {
+	return raiseDateStr;
+}
+
+public void setRaiseDateStr(String raiseDateStr) {
+	this.raiseDateStr = raiseDateStr;
+}
+
+public Date getCloseDate() {
+	return closeDate;
+}
+
+public void setCloseDate(Date closeDate) {
+	this.closeDate = closeDate;
+}
+
+public String getCloseDateStr() {
+	return closeDateStr;
+}
+
+public void setCloseDateStr(String closeDateStr) {
+	this.closeDateStr = closeDateStr;
+}
+
+public String getReqStatus() {
+	return reqStatus;
+}
+
+public void setReqStatus(String reqStatus) {
+	this.reqStatus = reqStatus;
+}
+
+public int getReqStatusId() {
+	return reqStatusId;
+}
+
+public void setReqStatusId(int reqStatusId) {
+	this.reqStatusId = reqStatusId;
+}
+
+public String getClosureReason() {
+	return closureReason;
+}
+
+public void setClosureReason(String closureReason) {
+	this.closureReason = closureReason;
+}
+
+public String getUserName() {
+	return userName;
+}
+
+public void setUserName(String userName) {
+	this.userName = userName;
+}	
+}
