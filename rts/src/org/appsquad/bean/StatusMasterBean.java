991c1d1499484272eb1d4d7d087b26c59d833d06
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-21 14:08:48 +0530

    #

---
+++ rts/src/org/appsquad/bean/StatusMasterBean.java
@@ -0,0 +1,21 @@
+package org.appsquad.bean;
+
+public class StatusMasterBean {
+	
+	private int statusId;
+	private String status;
+	
+	/***************************************************************************************************/
+	public int getStatusId() {
+		return statusId;
+	}
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
