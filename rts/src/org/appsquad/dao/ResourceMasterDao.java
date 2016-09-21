126f8267338d32968a1330124dced0005fa95c46
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-21 14:03:40 +0530

    #changes

--- rts/src/org/appsquad/dao/ResourceMasterDao.java
+++ rts/src/org/appsquad/dao/ResourceMasterDao.java
@@ -1,5 +1,381 @@
 package org.appsquad.dao;
 
-public class ResourceMasterDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.apache.log4j.Logger;
+import org.appsquad.bean.ClientInformationBean;
+import org.appsquad.bean.CountryBean;
+import org.appsquad.bean.ResourceMasterBean;
+import org.appsquad.bean.SkillsetMasterbean;
+import org.appsquad.bean.StateBean;
+import org.appsquad.bean.StatusMasterBean;
+import org.appsquad.database.DbConnection;
+import org.appsquad.sql.ClientInformationsql;
+import org.appsquad.sql.ResourceMasterSql;
+import org.appsquad.utility.Pstm;
+import org.zkoss.zul.Messagebox;
+
+public class ResourceMasterDao {
+	final static Logger logger=Logger.getLogger(ResourceMasterDao.class);
+	
+	public static ArrayList<StateBean> onLoadState(){
+		ArrayList<StateBean> stateList = new ArrayList<StateBean>();
+		Connection connection = null;
+		try {
+			connection = DbConnection.createConnection();
+			sql_connection:{
+				try {
+					
+					//1st SQL block
+					sql_fetch:{
+					   PreparedStatement preparedStatement = null;
+					   try {
+						   preparedStatement = Pstm.createQuery(connection, ClientInformationsql.stateQuery, null);
+							
+							ResultSet resultSet = preparedStatement.executeQuery();
+							while (resultSet.next()) {
+								StateBean bean = new StateBean();
+								bean.setStateId(resultSet.getInt("state_id"));
+								bean.setStateName(resultSet.getString("state_name").toUpperCase());
+								stateList.add(bean);
+							}  
+						} finally{
+							if(preparedStatement!=null){
+								preparedStatement.close();
+							}
+						}
+				    }
+				} catch (Exception e) {
+					e.printStackTrace();
+					logger.error(e);
+					logger.error(e);
+				}finally{
+					if(connection!=null){
+						connection.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+			logger.error(e);
+		}
+		return stateList;	
+	}
+	
+	public static ArrayList<CountryBean> onLoadCountry(){
+		ArrayList<CountryBean> countryList = new ArrayList<CountryBean>();
+		Connection connection = null;
+		try {
+			connection = DbConnection.createConnection();
+			sql_connection:{
+				try {
+					
+					//1st SQL block
+					sql_fetch:{
+					   PreparedStatement preparedStatement = null;
+					   try {
+						   preparedStatement = Pstm.createQuery(connection, ClientInformationsql.countryQuery, null);
+							
+							ResultSet resultSet = preparedStatement.executeQuery();
+							while (resultSet.next()) {
+								CountryBean bean = new CountryBean();
+								bean.setCountryId(resultSet.getInt("country_id"));
+								bean.setCountryName(resultSet.getString("country_name").toUpperCase());
+								countryList.add(bean);
+							}  
+						} finally{
+							if(preparedStatement!=null){
+								preparedStatement.close();
+							}
+						}
+				    }
+				} catch (Exception e) {
+					e.printStackTrace();
+					logger.error(e);
+					logger.error(e);
+				}finally{
+					if(connection!=null){
+						connection.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+			logger.error(e);
+		}
+		return countryList;
+	}
+	
+	public static ArrayList<SkillsetMasterbean> onLoadSkill(){
+		ArrayList<SkillsetMasterbean> skillList = new ArrayList<SkillsetMasterbean>();
+		Connection connection = null;
+		try {
+			connection = DbConnection.createConnection();
+			sql_connection:{
+				try {
+					
+					//1st SQL block
+					sql_fetch:{
+					   PreparedStatement preparedStatement = null;
+					   try {
+						   preparedStatement = Pstm.createQuery(connection, ClientInformationsql.skillSetQuery, null);
+							
+							ResultSet resultSet = preparedStatement.executeQuery();
+							while (resultSet.next()) {
+								SkillsetMasterbean masterbean = new SkillsetMasterbean();
+								masterbean.setId(resultSet.getInt("id"));
+								masterbean.setSkillset(resultSet.getString("master_skill_set_name"));
+								masterbean.setSkillsetdetails(resultSet.getString("skill_set_details"));
+								
+								skillList.add(masterbean);
+							}  
+						} finally{
+							if(preparedStatement!=null){
+								preparedStatement.close();
+							}
+						}
+				    }
+				} catch (Exception e) {
+					e.printStackTrace();
+					logger.error(e);
+					logger.error(e);
+				}finally{
+					if(connection!=null){
+						connection.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+			logger.error(e);
+		}
+		return skillList;	
+	}
+	
+	public static ArrayList<StatusMasterBean> onLoadStatus(){
+		ArrayList<StatusMasterBean> statusList = new ArrayList<StatusMasterBean>();
+		Connection connection = null;
+		try {
+			connection = DbConnection.createConnection();
+			sql_connection:{
+				try {
+					
+					//1st SQL block
+					sql_fetch:{
+					   PreparedStatement preparedStatement = null;
+					   try {
+						   preparedStatement = Pstm.createQuery(connection, ClientInformationsql.statusSetQuery, null);
+							
+							ResultSet resultSet = preparedStatement.executeQuery();
+							while (resultSet.next()) {
+								StatusMasterBean bean = new StatusMasterBean();
+								bean.setStatusId(resultSet.getInt("id"));
+								bean.setStatus(resultSet.getString("master_status_name"));
+								
+								statusList.add(bean);
+							}  
+						} finally{
+							if(preparedStatement!=null){
+								preparedStatement.close();
+							}
+						}
+				    }
+				} catch (Exception e) {
+					e.printStackTrace();
+					logger.error(e);
+					logger.error(e);
+				}finally{
+					if(connection!=null){
+						connection.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+			logger.error(e);
+		}
+		return statusList;	
+	}
+	
+	public static void insertClientData(ResourceMasterBean resourceMasterBean){
+		boolean isSaved = false;
+		Connection connection = null;
+		try {
+			connection = DbConnection.createConnection();
+			sql_connection:{
+				try {
+					
+					//1st SQL block
+					sql_insert:{
+					    PreparedStatement preparedStatementInsert = null;
+					    try {
+					    	preparedStatementInsert = Pstm.createQuery(connection, 
+									ResourceMasterSql.insertResourceQuery, Arrays.asList(resourceMasterBean.getName().toUpperCase(),resourceMasterBean.getSurName().toUpperCase(),
+											                                              resourceMasterBean.getYearOfExperience(),resourceMasterBean.getAddress().toUpperCase(),resourceMasterBean.getEmailId().toUpperCase(),
+											                                              resourceMasterBean.getUserId(),resourceMasterBean.getSkillsetMasterbean().getId(),resourceMasterBean.getStateBean().getStateId(),resourceMasterBean.getCountryBean().getCountryId(),
+											                                              resourceMasterBean.getPicCode().toUpperCase(),resourceMasterBean.getContactNumber().toUpperCase(),resourceMasterBean.getStatusMasterBean().getStatusId(),
+											                                              resourceMasterBean.getCtc(),resourceMasterBean.getSkillsetMasterbean().getSkillset().toUpperCase(),
+											                                              resourceMasterBean.getStatusMasterBean().getStatus().toUpperCase(),resourceMasterBean.getCountryBean().getCountryName().toUpperCase(),
+											                                              resourceMasterBean.getStateBean().getStateName().toUpperCase()));
+					    	
+					    	logger.info("Inserting Resource Data Into Table: "+preparedStatementInsert.unwrap(PreparedStatement.class));
+							int i = preparedStatementInsert.executeUpdate();
+							if(i>0){
+								isSaved = true;	
+							}
+						} finally{
+							if(preparedStatementInsert!=null){
+								preparedStatementInsert.close();
+							}
+						}
+				    }
+					if( isSaved){
+						Messagebox.show(" Resource Details Saved successfully!","Information",Messagebox.OK,Messagebox.INFORMATION);
+					}else{
+						Messagebox.show(" Resource Details failed due to internal error!","ERROR",Messagebox.OK,Messagebox.ERROR);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					logger.error(e);
+					logger.error(e);
+				}finally{
+					if(connection!=null){
+						connection.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+			logger.error(e);
+		}
+	}
+	
+	public static ArrayList<ResourceMasterBean> onLoadResourceDeatils(){
+		ArrayList<ResourceMasterBean> resourceList = new ArrayList<ResourceMasterBean>();
+		Connection connection = null;
+		try {
+			connection = DbConnection.createConnection();
+			sql_connection:{
+				try {
+					
+					//1st SQL block
+					sql_fetch:{
+					   PreparedStatement preparedStatement = null;
+					   try {
+						   preparedStatement = Pstm.createQuery(connection, ResourceMasterSql.fetchResourceQyery, null);
+							
+							ResultSet resultSet = preparedStatement.executeQuery();
+							while (resultSet.next()) {
+								ResourceMasterBean bean = new ResourceMasterBean();
+								bean.setResourceId(resultSet.getInt("id"));
+								bean.setName(resultSet.getString("res_name"));
+								bean.setSurName(resultSet.getString("res_surname"));
+								bean.setYearOfExperience(resultSet.getInt("res_experience"));
+								bean.getSkillsetMasterbean().setSkillset(resultSet.getString("rts_skill_name"));
+								bean.setAddress(resultSet.getString("res_address"));
+								bean.getStateBean().setStateName(resultSet.getString("rts_state_name"));
+								bean.getCountryBean().setCountryName(resultSet.getString("rts_country_name"));
+								bean.setPicCode(resultSet.getString("rts_pincode"));
+								bean.setContactNumber(resultSet.getString("rts_contact_no"));
+								bean.setEmailId(resultSet.getString("res_emailid"));
+								bean.getStatusMasterBean().setStatus(resultSet.getString("rts_state_name"));
+								bean.setCtc(resultSet.getDouble("rts_ctc"));
+								bean.getSkillsetMasterbean().setId(resultSet.getInt("rts_skill_id"));
+								bean.getStateBean().setStateId(resultSet.getInt("rts_state_id"));
+								bean.getCountryBean().setCountryId(resultSet.getInt("rts_country_id"));
+								bean.getStatusMasterBean().setStatusId(resultSet.getInt("rts_status_id"));
+								
+								resourceList.add(bean);
+							}  
+						} finally{
+							if(preparedStatement!=null){
+								preparedStatement.close();
+							}
+						}
+				    }
+				} catch (Exception e) {
+					e.printStackTrace();
+					logger.error(e);
+					logger.error(e);
+				}finally{
+					if(connection!=null){
+						connection.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+			logger.error(e);
+		}
+		return resourceList;
+	}
+	
+	public static boolean updateResourceData(ResourceMasterBean resourceMasterBean){
+		boolean isUpdated = false;
+		Connection connection = null;
+		try {
+			connection = DbConnection.createConnection();
+			sql_connection:{
+				try {
+					
+					//1st SQL block
+					sql_insert:{
+					    PreparedStatement preparedStatementInsert = null;
+					    try {
+					    	preparedStatementInsert = Pstm.createQuery(connection, 
+									ResourceMasterSql.updateResourceQuery, Arrays.asList(resourceMasterBean.getName().toUpperCase(),resourceMasterBean.getSurName().toUpperCase(),
+											                                              resourceMasterBean.getYearOfExperience(),resourceMasterBean.getAddress().toUpperCase(),resourceMasterBean.getEmailId().toUpperCase(),
+											                                              resourceMasterBean.getSkillsetMasterbean().getId(),resourceMasterBean.getStateBean().getStateId(),resourceMasterBean.getCountryBean().getCountryId(),
+											                                              resourceMasterBean.getPicCode().toUpperCase(),resourceMasterBean.getContactNumber().toUpperCase(),resourceMasterBean.getStatusMasterBean().getStatusId(),
+											                                              resourceMasterBean.getCtc(),resourceMasterBean.getSkillsetMasterbean().getSkillset().toUpperCase(),
+											                                              resourceMasterBean.getStatusMasterBean().getStatus().toUpperCase(),resourceMasterBean.getCountryBean().getCountryName().toUpperCase(),
+											                                              resourceMasterBean.getStateBean().getStateName().toUpperCase(),resourceMasterBean.getResourceId()));
+					    	
+					    	
+					    	System.out.println(preparedStatementInsert);
+					    	logger.info("Inserting Client Data Into Table: "+preparedStatementInsert.unwrap(PreparedStatement.class));
+							int i = preparedStatementInsert.executeUpdate();
+							if(i>0){
+								isUpdated = true;	
+							}
+						} finally{
+							if(preparedStatementInsert!=null){
+								preparedStatementInsert.close();
+							}
+						}
+				    }
+				
+					if( isUpdated){
+						Messagebox.show(" Resource Details Updated successfully!","Information",Messagebox.OK,Messagebox.INFORMATION);
+					}else{
+						Messagebox.show(" Resource Details failed due to internal error!","ERROR",Messagebox.OK,Messagebox.ERROR);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					logger.error(e);
+					logger.error(e);
+				}finally{
+					if(connection!=null){
+						connection.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+			logger.error(e);
+		}
+		return isUpdated;
+	}
+	
 }
--- rts/src/org/appsquad/sql/ClientInformationsql.java
+++ rts/src/org/appsquad/sql/ClientInformationsql.java
@@ -2,17 +2,21 @@ package org.appsquad.sql;
 
 public class ClientInformationsql {
 	
-	public static String stateQuery = "select state_id,state_name from rts_state_master ";
+	public static final String stateQuery = "select state_id,state_name from rts_state_master ";
 	
-	public static String countryQuery = "select country_id,country_name from rts_country_master ";
+	public static final String countryQuery = "select country_id,country_name from rts_country_master ";
 	
-	public static String insertClientInfo = " INSERT INTO rts_clients_master(name,surname, companyname, officeaddress, state, "
+	public static final String skillSetQuery = "select * from rts_skill_master ";
+	
+	public static final String statusSetQuery = "select * from rts_status_master ";
+	
+	public static final String insertClientInfo = " INSERT INTO rts_clients_master(name,surname, companyname, officeaddress, state, "
                                            +" country, zipcode, contactno, emailid, created_by,state_id, country_id) "
                                            +" VALUES (?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?) ";
 	
-	public static String fetchClientDeatils = "select * from rts_clients_master ";
+	public static final String fetchClientDeatils = "select * from rts_clients_master ";
 	
-	public static String clientDetailsUpdate = "UPDATE rts_clients_master set name = ?,companyname = ?,"
+	public static final String clientDetailsUpdate = "UPDATE rts_clients_master set name = ?,companyname = ?,"
 			                                 + "officeaddress = ?,state = ?,country = ?,zipcode = ?,contactno = ?,emailid = ?,state_id= ?,"
 			                                 + "country_id = ? where id = ? ";
 
