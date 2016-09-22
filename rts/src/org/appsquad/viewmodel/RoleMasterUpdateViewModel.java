cda2902ba5e10e35990896f8351f14ce1cd0ed9f
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-22 13:29:16 +0530

    #

---
+++ rts/src/org/appsquad/viewmodel/RoleMasterUpdateViewModel.java
@@ -0,0 +1,154 @@
+package org.appsquad.viewmodel;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import org.appsquad.bean.ClientInformationBean;
+import org.appsquad.bean.RoleMasterBean;
+import org.appsquad.bean.RollDropDownBean;
+import org.appsquad.dao.ClientInformationDao;
+import org.appsquad.dao.RoleMasterDao;
+import org.appsquad.service.ClientInformationService;
+import org.appsquad.service.RoleMasterService;
+import org.zkoss.bind.BindUtils;
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
+
+public class RoleMasterUpdateViewModel {
+	RoleMasterBean roleMasterBean = new RoleMasterBean();
+	private RoleMasterBean masterBean;
+	
+	private Connection connection = null;
+	private Session sessions = null;
+	private String userName ;
+	private String userId;
+	@Wire("#winRoleUpdate")
+	private Window winRoleUpdate;
+	private boolean flag = false;
+	private Integer count;
+	private Integer uId;
+
+	private ArrayList<RollDropDownBean> roleList = new ArrayList<RollDropDownBean>();
+
+	
+	@AfterCompose
+	public void initSetup(@ContextParam(ContextType.VIEW) Component view,@ExecutionArgParam("roleDetails") RoleMasterBean bean)
+			throws Exception {
+		Selectors.wireComponents(view, this, false);
+		masterBean = bean;
+		sessions = Sessions.getCurrent();
+		roleList = RoleMasterDao.onLoadRoleDropDownDeatils();
+		masterBean.setVisibilityUpdateButton(true);
+		uId = masterBean.getUserprofileBean().getId();
+		masterBean.setuId(uId);
+		System.out.println(masterBean.getuId());
+	}
+	
+	@Command
+	@NotifyChange("*")
+	public void onSelectRollName(){
+		System.out.println("ROLE ID IS :"+masterBean.getDownBean().getRollId());
+		System.out.println("MAPPER ID IS :"+masterBean.getMapperId());
+		masterBean.setVisibilityUpdateButton(false);
+	}
+	
+	@Command
+	@NotifyChange("*")
+	public void onClickUpdateButton(){
+		count = RoleMasterDao.onLoadCountTabDeatils(masterBean);
+		System.out.println(count);
+		if(count>0){
+			Messagebox.show(" This User Name And Role Name Mapping Already Done!","Exclamation",Messagebox.OK,Messagebox.EXCLAMATION);
+		}else{
+			flag = RoleMasterService.updateAssignRoleData(masterBean);
+			if(flag){
+				winRoleUpdate.detach();
+				BindUtils.postGlobalCommand(null, null, "globalRoleDetailsUpdate", null);
+			}	
+		}
+	}
+	
+	/**************************************************************************************************************************************/
+	
+	public RoleMasterBean getRoleMasterBean() {
+		return roleMasterBean;
+	}
+	public void setRoleMasterBean(RoleMasterBean roleMasterBean) {
+		this.roleMasterBean = roleMasterBean;
+	}
+	public RoleMasterBean getMasterBean() {
+		return masterBean;
+	}
+	public void setMasterBean(RoleMasterBean masterBean) {
+		this.masterBean = masterBean;
+	}
+	public Connection getConnection() {
+		return connection;
+	}
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	public Session getSessions() {
+		return sessions;
+	}
+	public void setSessions(Session sessions) {
+		this.sessions = sessions;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Window getWinRoleUpdate() {
+		return winRoleUpdate;
+	}
+	public void setWinRoleUpdate(Window winRoleUpdate) {
+		this.winRoleUpdate = winRoleUpdate;
+	}
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	public ArrayList<RollDropDownBean> getRoleList() {
+		return roleList;
+	}
+	public void setRoleList(ArrayList<RollDropDownBean> roleList) {
+		this.roleList = roleList;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public Integer getuId() {
+		return uId;
+	}
+
+	public void setuId(Integer uId) {
+		this.uId = uId;
+	}
+}
