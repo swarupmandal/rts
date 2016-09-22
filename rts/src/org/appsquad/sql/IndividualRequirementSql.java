84f21a9de45cc3b618e560e1eab7ffc56d8b5a30
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-22 17:57:03 +0530

    #

---
+++ rts/src/org/appsquad/sql/IndividualRequirementSql.java
@@ -0,0 +1,6 @@
+package org.appsquad.sql;
+
+public class IndividualRequirementSql {
+	public static final String fetchReqIdList = "select * from rts_requirement_master where is_delete = 'N' ";
+
+}
