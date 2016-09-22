036ab556311b4173c9083f04ac7b166a235aa768
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-22 13:29:41 +0530

    #

---
+++ rts/src/org/appsquad/bean/RollDropDownBean.java
@@ -0,0 +1,98 @@
+package org.appsquad.bean;
+
+public class RollDropDownBean {
+	private String rollid;
+	private String roll;
+	private String user;
+	private String userid;
+	private int rollId;
+	private boolean visibilityRoleTextBox = true;
+	private boolean visibilityEditButton = false;
+	private boolean visibilitySaveButton = false;
+	private boolean visibilityDeleteButton = false;
+	
+	private UserprofileBean userprofileBean = new UserprofileBean();
+
+	public String getRollid() {
+		return rollid;
+	}
+
+	public void setRollid(String rollid) {
+		this.rollid = rollid;
+	}
+
+	public String getRoll() {
+		return roll;
+	}
+
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public int getRollId() {
+		return rollId;
+	}
+
+	public void setRollId(int rollId) {
+		this.rollId = rollId;
+	}
+
+	public boolean isVisibilityRoleTextBox() {
+		return visibilityRoleTextBox;
+	}
+
+	public void setVisibilityRoleTextBox(boolean visibilityRoleTextBox) {
+		this.visibilityRoleTextBox = visibilityRoleTextBox;
+	}
+
+	public boolean isVisibilityEditButton() {
+		return visibilityEditButton;
+	}
+
+	public void setVisibilityEditButton(boolean visibilityEditButton) {
+		this.visibilityEditButton = visibilityEditButton;
+	}
+
+	public boolean isVisibilitySaveButton() {
+		return visibilitySaveButton;
+	}
+
+	public void setVisibilitySaveButton(boolean visibilitySaveButton) {
+		this.visibilitySaveButton = visibilitySaveButton;
+	}
+
+	public boolean isVisibilityDeleteButton() {
+		return visibilityDeleteButton;
+	}
+
+	public void setVisibilityDeleteButton(boolean visibilityDeleteButton) {
+		this.visibilityDeleteButton = visibilityDeleteButton;
+	}
+
+	public UserprofileBean getUserprofileBean() {
+		return userprofileBean;
+	}
+
+	public void setUserprofileBean(UserprofileBean userprofileBean) {
+		this.userprofileBean = userprofileBean;
+	} 
+	
+	
+
+}
