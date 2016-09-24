2105422959cf29d9edd993d6e4f5e47bb433610d
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-24 15:41:58 +0530

    #

--- rts/src/org/appsquad/sql/ResourceAllocationSql.java
+++ rts/src/org/appsquad/sql/ResourceAllocationSql.java
@@ -9,5 +9,10 @@ public class ResourceAllocationSql {
     public static final String fetchResourceDetails = "select id,res_name,res_surname,res_experience,res_address,res_emailid from rts_resource_master where non_allocable_or_not = 'N' ";
     public static final String fetchRequiredResourceNumberPerAllocated = "select num_of_per_res_allocated from vw_req_skill_details where req_client_id = ? and r_id = ? ";
     public static final String fetchRequiredResourceNumberConAllocated = "select num_of_con_res_allocated from vw_req_skill_details where req_client_id = ? and r_id = ? ";
-    
+    public static final String updateResourceTableSql = "update rts_resource_master set non_allocable_or_not = 'Y' where id = ? ";
+    public static final String updateResourceTablePerSql = "update rts_requirement_master set num_of_per_res_allocated = ? where req_client_id = ? and r_id = ? "; 
+    public static final String updateResourceTableConSql = "update rts_requirement_master set num_of_con_res_allocated = ? where req_client_id = ? and r_id = ? "; 
+    public static final String insertIntoMapperSql = "INSERT INTO rts_req_resource_mapper(req_id, resource_id, client_id, created_by) VALUES (?, ?, ?,?) ";
+    public static final String insertIntoTrackingHistoryTableSql = "INSERT INTO rts_req_res_status_tracking(r_id, resource_id, status_id,created_by) VALUES (?, ?,?, ?) "; 
+    public static final String fetchStatusIdSql = "select id from rts_status_master where master_status_name = 'INITIAL ' ";
 }
