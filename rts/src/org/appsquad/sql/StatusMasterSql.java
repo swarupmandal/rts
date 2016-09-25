f086d4ac8124bc6c69f1c4d8db0d23676cb7a752
ProlayjitDutta <prolayjitdutta@gmail.com>
2016-09-26 01:44:05 +0530

    #

--- rts/src/org/appsquad/sql/StatusMasterSql.java
+++ rts/src/org/appsquad/sql/StatusMasterSql.java
@@ -4,4 +4,5 @@ public class StatusMasterSql {
 	public static final String insertStatusQuery = "INSERT INTO rts_status_master(created_by,master_status_name)VALUES (?,?) ";
     public static final String fetchStatusQuery = "select * from rts_status_master where is_delete = 'N' ";
     public static final String deleteStatusQuery = "update rts_status_master set is_delete = 'D' where id = ? ";
+    public static final String countStatusSql = "select count(*) from rts_status_master where master_status_name = ? ";
 }
