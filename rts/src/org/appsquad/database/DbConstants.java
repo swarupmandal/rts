f857e899ac3dba9d2b6f2f021197719477f9a556
lenovo <lenovo@DESKTOP-0V5MQO1>
2016-10-03 15:40:34 +0530

    @

--- rts/src/org/appsquad/database/DbConstants.java
+++ rts/src/org/appsquad/database/DbConstants.java
@@ -4,8 +4,8 @@ public class DbConstants {
 	public static final String DRIVERCLASS = "org.postgresql.Driver";
 	public static final String DBNAME="rts";
 	//public static final String JDBCURL = "jdbc:postgresql://192.168.1.21:5432/"+DBNAME;
-	public static final String JDBCURL = "jdbc:postgresql://192.168.1.41:5432/"+DBNAME;
-	//public static final String JDBCURL = "jdbc:postgresql://localhost:5432/"+DBNAME;
+	//public static final String JDBCURL = "jdbc:postgresql://192.168.1.41:5432/"+DBNAME;
+	public static final String JDBCURL = "jdbc:postgresql://localhost:5432/"+DBNAME;
 	public static final String USERNAME = "postgres";
 	public static final String PASSWORD = "password";
 }
