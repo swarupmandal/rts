94514e676876328e23cabd4be1449461e89e17a4
somnathdutta <somnathdutta048@gmail.com>
2016-12-30 13:14:44 +0530

    #align

--- rts/src/org/appsquad/utility/DownloadPdf.java
+++ rts/src/org/appsquad/utility/DownloadPdf.java
@@ -12,34 +12,34 @@ import org.zkoss.zul.Messagebox;
 public class DownloadPdf {
 	public static void download(String pdfNamewithPath, String fileName) throws IOException{
 		System.out.println("Download method calling..");
-		  String path=pdfNamewithPath.replace('\\','/');
-		  //Messagebox.show(path);
-		  byte[] ba1 = new byte[102400];
-		  File myFile = new File(path );
-		  FileInputStream fis = new FileInputStream(myFile);
-		  int baLength;
-		  ByteArrayOutputStream bios = new ByteArrayOutputStream();
-		  try {
-		   try{
-		    while ( (baLength = fis.read(ba1)) != -1) {
-		     bios.write(ba1, 0, baLength);
-		    }
+		String path=pdfNamewithPath.replace('\\','/');
+		//Messagebox.show(path);
+		byte[] ba1 = new byte[102400];
+		File myFile = new File(path );
+		FileInputStream fis = new FileInputStream(myFile);
+		int baLength;
+		ByteArrayOutputStream bios = new ByteArrayOutputStream();
+		try {
+			try{
+				while ( (baLength = fis.read(ba1)) != -1) {
+					bios.write(ba1, 0, baLength);
+				}
 
-		   } catch (Exception e1) {}  
-		   finally {
-		    fis.close();
-		   }
-		   final AMedia amedia = new AMedia(fileName, "pdf", "application/pdf", bios.toByteArray());
-		   Filedownload.save(amedia);
-		  } catch (Exception ex) {
-		   ex.printStackTrace();
-		  } finally {
-		   File xlsFile = new File(path);
-		   if (xlsFile.exists()) {
-		    //FileUtils.forceDelete(xlsFile);
-		    xlsFile.delete();
-		   }
+			} catch (Exception e1) {}  
+			finally {
+				fis.close();
+			}
+			final AMedia amedia = new AMedia(fileName, "pdf", "application/pdf", bios.toByteArray());
+			Filedownload.save(amedia);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			File xlsFile = new File(path);
+			if (xlsFile.exists()) {
+				//FileUtils.forceDelete(xlsFile);
+				xlsFile.delete();
+			}
 
-		  }
-		 }
+		}
+	}
 }
