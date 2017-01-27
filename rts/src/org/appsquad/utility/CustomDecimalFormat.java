aa5a30c7182ee8f30124228a695c7da3c27b9c87
ProlayjitDutta <prolayjitdutta@gmail.com>
2017-01-27 18:15:18 +0530

    #changes

---
+++ rts/src/org/appsquad/utility/CustomDecimalFormat.java
@@ -0,0 +1,29 @@
+package org.appsquad.utility;
+
+import java.text.DecimalFormat;
+
+public class CustomDecimalFormat {
+	public static Float floatFormat(Float value){
+		if(value != null){
+			return Float.valueOf(new DecimalFormat("######.##").format(value) );
+		}else{
+			return null;
+		}
+	}
+	
+	public static Double doubleFormat(Float value){
+		if(value != null){
+			return Double.valueOf(new DecimalFormat("######.##").format(value) );
+		}else{
+			return null;
+		}
+	}
+	
+	public static Double doubleFormat(Double value){
+		if(value != null){
+			return Double.valueOf(new DecimalFormat("######.###").format(value) );
+		}else{
+			return null;
+		}
+	}
+}
